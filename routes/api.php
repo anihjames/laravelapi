af335ae63071e4bfacfa4f4083843a293e639fa2
jobsworks <uchennaanih16@gmail.com>
2019-12-06 14:18:31 +0100

    added passport token

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::prefix('v1')->group(function(){
+    Route::post('register', 'API\RegisterController@register');
+    Route::post('login', 'API\RegisterController@login');
+
+    Route::group(['middleware'=>'auth:api'], function(){
+        Route::resource('products', 'API\ProductController');
+    });
 });
+
+   
+
